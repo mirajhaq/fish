a80b2620e0d2becbba77b26042785c46974b9229
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2025-02-28 17:39:36 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import AboutDetails from "./components/AboutDetails";
 import Projects from "./components/Projects";
 import Shop from "./components/Shop"; // Import Shop component
 import ShopDetails from "./components/ShopDetails"; // Import ShopDetails component
-import Services from "./components/Services"; // Import ShopDetails component
-import Contact from "./components/Contact"; // Import ShopDetails component
+import Services from "./components/Services"; // Import Services component
+import Contact from "./components/Contact"; // Import Contact component
 
 
 function App() {
@@ -18,15 +18,20 @@ function App() {
         <NavBar />
         <Fishtank />
         <Routes>
-          <Route path="/" element={<>
-            <About />
-            <Projects />
-            <Shop />
-            <Services />
-            <Contact />
-          </>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <About />
+                <Projects />
+                <Shop />
+                <Services />
+                <Contact />
+              </>
+            }
+          />
           <Route path="/about-details" element={<AboutDetails />} />
-          <Route path="/shop-details" element={<ShopDetails />} /> {/* Add route for ShopDetails */}
+          <Route path="/shop-details" element={<ShopDetails />} /> {/* Route for ShopDetails */}
         </Routes>
       </div>
     </Router>
